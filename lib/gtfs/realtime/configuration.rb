7d4567549140ad9891db5c7a15ad9d95fe6476fd
Ryan Laughlin <rofreg@gmail.com>
2016-12-02 19:23:44 -0500

    Switch config.database_path to config.database_url

--- lib/gtfs/realtime/configuration.rb
+++ lib/gtfs/realtime/configuration.rb
@@ -1,11 +1,11 @@
 module GTFS
   class Realtime
     class Configuration
-      attr_accessor :static_feed, :trip_updates_feed, :vehicle_positions_feed, :service_alerts_feed, :database_path
+      attr_accessor :static_feed, :trip_updates_feed, :vehicle_positions_feed, :service_alerts_feed, :database_url
 
-      def database_path=(new_path)
-        @database_path = new_path
-        ActiveRecord::Base.establish_connection(@database_path)
+      def database_url=(new_path)
+        @database_url = new_path
+        ActiveRecord::Base.establish_connection(@database_url)
       end
     end
   end
